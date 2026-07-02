542a287c5b05143d371d788cff49323202cb50d9
pulkitpareek1915 <145360313+pulkitpareek1915@users.noreply.github.com>
2026-07-03 02:41:07 +0530

    Time: 0 ms (100%), Space: 7.8 MB (77.41%) - LeetHub

---
+++ 1201-ugly-number-iii/1201-ugly-number-iii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    long long count(int a, int b, int c, long long mid)
+{
+    long long ab = lcm(1LL * a, 1LL * b);
+    long long ac = lcm(1LL * a, 1LL * c);
+    long long bc = lcm(1LL * b, 1LL * c);
+    long long abc = lcm(ab, 1LL * c);
+
+    return mid / a + mid / b + mid / c
+         - mid / ab - mid / ac - mid / bc
+         + mid / abc;
+}
+
+    int nthUglyNumber(int n, int a, int b, int c) {
+        long long start = 1;
+        long long end = 2000000000;
+
+        while(start <= end)
+        {
+            long long mid = start + (end - start) / 2;
+
+            if(count(a, b, c, mid) < n)
+            {
+                start = mid + 1;
+            }
+            else
+            {
+                end = mid - 1;
+            }
+        }
+
+        return start;
+    }
+};
