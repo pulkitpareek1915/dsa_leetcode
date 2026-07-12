f7eb183274699f80aa842d7f1a0c3aac00c6962d
pulkitpareek1915 <145360313+pulkitpareek1915@users.noreply.github.com>
2026-07-13 03:04:43 +0530

    Time: 0 ms (100%), Space: 12.3 MB (42.5%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,40 +1,20 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> ans(n+m,0);
-        int left=0;
         int right=0;
-        int index=0;
-        while(left < m && right < n)
+        int left=m-1;
+    
+        while (left >= 0 && right < n && nums1[left] > nums2[right])
         {
-            if(nums1[left]>nums2[right])
-            {
-                ans[index]=nums2[right];
-                index++;
-                right++;
-            }
-            else{
-                ans[index]=nums1[left];
-                index++;
-                left++;
-            }
-        }
-        while(left<m)
-        {
-            ans[index]=nums1[left];
-            index++;
-            left++;
-        }
-        while(right<n)
-        {
-            ans[index]=nums2[right];
-            index++;
+            swap(nums1[left],nums2[right]);
             right++;
+            left--;
         }
-        for(int i=0;i<n+m;i++)
+        sort(nums1.begin(),nums1.begin()+m);
+        sort(nums2.begin(),nums2.begin()+n);
+        for(int i=m;i<n+m;i++)
         {
-             nums1[i]=ans[i];
-            
+            nums1[i]=nums2[i-m];
         }
     }
 };
