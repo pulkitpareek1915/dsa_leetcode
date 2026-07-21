fe3866053ede4aadf783ae0a9cb687a17578d535
pulkitpareek1915 <145360313+pulkitpareek1915@users.noreply.github.com>
2026-07-21 23:11:47 +0530

    Time: 71 ms (5.12%), Space: 42.5 MB (15.36%) - LeetHub

---
+++ 2606-find-the-substring-with-maximum-cost/2606-find-the-substring-with-maximum-cost.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int maximumCostSubstring(string s, string chars, vector<int>& vals) {
+        int n=s.size();
+        vector<int> nums(n,0);
+        vector<int> cha(26,-1001);
+        for(int i=0;i<chars.size();i++)
+        {
+            char c=chars[i];
+            int index=c-'a';
+            cha[index]=vals[i];
+        }
+        for(int i=0;i<26;i++)
+        {
+            if(cha[i]==-1001)
+            {
+                cha[i]=i+1;
+            }
+        }
+        for(int i=0;i<26;i++)
+        {
+            cout<<cha[i]<<" ";
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            nums[i]=cha[s[i]-'a'];
+        }
+        int sum=0;
+        int maxi=INT_MIN;
+        for(int i=0;i<n;i++)
+        {
+            sum+=nums[i];
+            if(sum<0)
+            {
+                sum=0;
+            }
+            maxi=max(maxi,sum);
+        }
+         return maxi;
+    }
+};
