2ef586e095fab18b66aabfaf7b1bb18b562851f4
pulkitpareek1915 <145360313+pulkitpareek1915@users.noreply.github.com>
2026-07-24 00:25:57 +0530

    Time: 0 ms (100%), Space: 30.3 MB (69.26%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        int n=nums1.size();
+       if(n>=1)
+       {
+           return true;
+       }
+        return false;
+    }
+};
