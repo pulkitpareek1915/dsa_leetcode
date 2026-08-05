265e3508fb951fe938ca848416264b02992c8037
pulkitpareek1915 <145360313+pulkitpareek1915@users.noreply.github.com>
2026-08-06 01:37:07 +0530

    Time: 30 ms (63.61%), Space: 19.3 MB (50.8%) - LeetHub

--- 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.cpp
+++ 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -1,49 +1,32 @@
 class Solution {
 public:
     int findTheLongestSubstring(string s) {
-        int n=s.size();
-        vector<int> vowel(n,0);
-        for(int i=0;i<n;i++)
-        {
-            if(s[i]=='a')
-            {
-                vowel[i]=2;
-            }
-            else if(s[i]=='e')
-            {
-                vowel[i]=4;
-            }
-            else if(s[i]=='i')
-            {
-                vowel[i]=8;
-            }
-            else if(s[i]=='o')
-            {
-                vowel[i]=16;
-            }
-            else if(s[i]=='u')
-            {
-                vowel[i]=32;
-            }
-            else{
-                continue;
-            }
+
+        unordered_map<int, int> mp;
+        mp[0] = -1;
+
+        int xr = 0;
+        int ans = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+
+            if (s[i] == 'a')
+                xr ^= 2;
+            else if (s[i] == 'e')
+                xr ^= 4;
+            else if (s[i] == 'i')
+                xr ^= 8;
+            else if (s[i] == 'o')
+                xr ^= 16;
+            else if (s[i] == 'u')
+                xr ^= 32;
+
+            if (mp.count(xr))
+                ans = max(ans, i - mp[xr]);
+            else
+                mp[xr] = i;
         }
-        unordered_map<int ,int> map;
-        map[0]=-1;
-        int maxlen=0;
-        int xor1=0;
-        for(int i=0;i<n;i++)
-        {
-            xor1=xor1^vowel[i];
-            if(map.count(xor1))
-            {
-                maxlen=max(maxlen,i-map[xor1]);
-            }
-            else{
-                map[xor1]=i;
-            }
-        }
-        return maxlen;
+
+        return ans;
     }
 };
