f333b9c3e681aa4b474c5af072b6f25542748363
pulkitpareek1915 <145360313+pulkitpareek1915@users.noreply.github.com>
2026-08-19 00:38:16 +0530

    Time: 201 ms (17.32%), Space: 230.3 MB (7.04%) - LeetHub

---
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool hasduplicate(string temp, string s)
+    {
+        vector<int> freq(26,0);
+
+        for(char c : temp)
+        {
+            freq[c-'a']++;
+            if(freq[c-'a'] > 1) return true;
+        }
+
+        for(char c : s)
+        {
+            freq[c-'a']++;
+            if(freq[c-'a'] > 1) return true;
+        }
+
+        return false;
+    }
+    int solve(int index,vector<string> arr,string temp,int n)
+    {
+        if(index>=n)
+        {
+            return temp.size();
+        }
+        int pick=0;
+        int notpick=0;
+        if(hasduplicate(temp,arr[index]))
+        {
+            notpick=solve(index+1,arr,temp,n);
+        }
+        else{
+            pick=solve(index+1,arr,temp+arr[index],n);
+            notpick=solve(index+1,arr,temp,n);
+        }
+        return max(pick,notpick);
+    }
+    int maxLength(vector<string>& arr) {
+        int n=arr.size();
+        string temp="";
+        return solve(0,arr,temp,n);
+    }
+};
