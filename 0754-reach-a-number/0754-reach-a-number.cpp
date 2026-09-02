307dda7ab68929dd7365c0c775b6b7d58a076931
pulkitpareek1915 <145360313+pulkitpareek1915@users.noreply.github.com>
2026-09-02 15:44:46 +0530

    Time: 0 ms (100%), Space: 7.9 MB (9.02%) - LeetHub

---
+++ 0754-reach-a-number/0754-reach-a-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reachNumber(int target) {
+        target = abs(target);
+
+        long long sum = 0;
+        int steps = 0;
+
+        while (sum < target || (sum - target) % 2 != 0) {
+            steps++;
+            sum += steps;
+        }
+
+        return steps;
+    }
+};
