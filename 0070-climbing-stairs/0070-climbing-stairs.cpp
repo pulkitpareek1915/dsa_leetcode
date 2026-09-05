834f81710d8a90c86844873b6e22be667f6898ea
pulkitpareek1915 <145360313+pulkitpareek1915@users.noreply.github.com>
2026-09-05 18:49:21 +0530

    Time: 0 ms (100%), Space: 8.7 MB (20.61%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
-   
+    int solve(int index,vector<int> &dp)
+    {
+        if(index==-1)
+        {
+            return 0;
+        }
+        if(index==0)
+        {
+            return 1;
+        }
+        if(dp[index]!=-1)
+        {
+            return dp[index];
+        }
+        return dp[index]=solve(index-1,dp)+solve(index-2,dp);
+    }
     int climbStairs(int n) {
         vector<int> dp(n+1,-1);
-        dp[0]=1;
-        dp[1]=1;
-        for(int i=2;i<=n;i++)
-        {
-            dp[i]=dp[i-1]+dp[i-2];
-        }
-        return dp[n];
+        return solve(n,dp);
     }
 };
